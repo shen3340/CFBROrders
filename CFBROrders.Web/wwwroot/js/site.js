c9244f024fb629ee62bfe0c60fd13cc960aa385e
shen3340 <shen3340@gmail.com>
2025-12-10 00:00:29 -0500

    #15 got all teamcolor CSS to show up correctly

--- CFBROrders.Web/wwwroot/js/site.js
+++ CFBROrders.Web/wwwroot/js/site.js
@@ -1,3 +1,4 @@
-﻿window.setTeamColor = function (color) {
+﻿window.setTeamColor = function (color, contrastColor) {
     document.documentElement.style.setProperty('--team-color', color);
+    document.documentElement.style.setProperty('--team-contrast-color', contrastColor);
 };
