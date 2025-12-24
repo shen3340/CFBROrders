863beeb7d4a7e1038d9407776758c7d971623b95
shen3340 <shen3340@gmail.com>
2025-12-24 12:01:04 -0600

    #36 added seasonal background effects #done

---
+++ CFBROrders.Web/Models/SettingOption.cs
@@ -0,0 +1,35 @@
+﻿namespace CFBROrders.Web.Models
+{
+    public enum SettingType
+    {
+        Boolean,
+        Select
+    }
+
+    public class SettingOption
+    {
+        public string Name { get; set; }
+        public string Label { get; set; }
+        public SettingType Type { get; set; }
+        public bool BoolValue { get; set; }
+        public string? SelectedValue { get; set; }
+        public List<string>? Options { get; set; }
+
+        public SettingOption(string name, string label)
+        {
+            Name = name;
+            Label = label;
+            Type = SettingType.Boolean;
+            BoolValue = false;
+        }
+
+        public SettingOption(string name, string label, List<string> options, string defaultValue)
+        {
+            Name = name;
+            Label = label;
+            Type = SettingType.Select;
+            Options = options;
+            SelectedValue = defaultValue;
+        }
+    }
+}
